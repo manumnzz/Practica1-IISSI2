1357739345f39d7c759cb03c71b240d0b2c35451
manu.mnzz <manbuzmun@alum.us.es>
2024-03-01 08:47:58 +0100

    modified:   DeliverUS-Backend/src/database/migrations/20210629195916-create-restaurant.js 	modified:   DeliverUS-Backend/src/models/Restaurant.js 	modified:   package-lock.json 	modified:   package.json

--- DeliverUS-Backend/src/database/migrations/20210629195916-create-restaurant.js
+++ DeliverUS-Backend/src/database/migrations/20210629195916-create-restaurant.js
@@ -6,9 +6,95 @@ module.exports = {
         autoIncrement: true,
         primaryKey: true,
         type: Sequelize.INTEGER
-      }
-      // TODO: Include the rest of the fields of the Restaurants table
+      },
 
+      name: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+
+      description: {
+        allowNull: true,
+        type: Sequelize.STRING
+      },
+
+      address: {
+        allowNull: true,
+        type: Sequelize.STRING
+      },
+
+      postalCode: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+
+      url: {
+        allowNull: true,
+        type: Sequelize.STRING
+      },
+
+      shippingCosts: {
+        allowNull: false,
+        type: Sequelize.DOUBLE
+      },
+
+      averageServiceMinutes: {
+        allowNull: true,
+        type: Sequelize.DOUBLE
+      },
+
+      email: {
+        allowNull: true,
+        type: Sequelize.STRING
+      },
+
+      phone: {
+        allowNull: true,
+        type: Sequelize.STRING
+      },
+
+      logo: {
+        allowNull: true,
+        type: Sequelize.STRING
+      },
+
+      heroImage: {
+        allowNull: true,
+        type: Sequelize.STRING
+      },
+
+      status: {
+        allowNull: false,
+        type: Sequelize.ENUM,
+        values: ['online', 'offline', 'closed', 'temporaly closed']
+      },
+
+      restaurantCategoryId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: {
+            tableName: 'RestaurantCategories'
+          },
+          key: 'id'
+        }
+      },
+
+      userId: {
+        allowNull: true,
+        type: Sequelize.STRING
+      },
+
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: new Date()
+      },
+
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: new Date()
+      }
     })
   },
   down: async (queryInterface, Sequelize) => {
--- DeliverUS-Backend/src/models/Restaurant.js
+++ DeliverUS-Backend/src/models/Restaurant.js
@@ -27,8 +27,18 @@ const loadModel = (sequelize, DataTypes) => {
     }
   }
   Restaurant.init({
-    // TODO: Include the rest of the properties of the Restaurant model
-
+    name: DataTypes.STRING,
+    description: DataTypes.STRING,
+    address: DataTypes.STRING,
+    postalCode: DataTypes.STRING,
+    url: DataTypes.STRING,
+    shippingCosts: DataTypes.DOUBLE,
+    averageServiceMinutes: DataTypes.DOUBLE,
+    email: DataTypes.STRING,
+    phone: DataTypes.STRING,
+    logo: DataTypes.STRING,
+    heroImage: DataTypes.STRING,
+    status: DataTypes.ENUM
   }, {
     sequelize,
     modelName: 'Restaurant'
